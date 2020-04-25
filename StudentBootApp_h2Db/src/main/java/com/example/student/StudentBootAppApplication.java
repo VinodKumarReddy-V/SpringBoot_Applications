4041f31b2bd5471f4c6cfe6319a30ca708e4a079
VinodKumarReddy V <61618854+VinodKumarReddy-V@users.noreply.github.com>
2020-04-25 21:56:24 +0530

    Add files via upload
    This is the application for spring boot demo to store data into H2-DB

---
+++ StudentBootApp_h2Db/src/main/java/com/example/student/StudentBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentBootAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Spring Boot Application Starts");
+		SpringApplication.run(StudentBootAppApplication.class, args);
+	}
+
+}
---
+++ StudentBootApp_h2Db/src/main/java/com/example/student/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.student.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.student.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+}
